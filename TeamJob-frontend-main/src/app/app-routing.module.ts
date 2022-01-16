8f8f04c86bad47009c88e8ee42326caa16a04c8b
mariem202 <mariemmhiri82@gmail.com>
2022-01-16 23:37:14 +0100

    Acc_gards

--- TeamJob-frontend-main/src/app/app-routing.module.ts
+++ TeamJob-frontend-main/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AcceilComponent } from './acceil/acceil.component';
+import { AcceuilGuard } from './acceuil.guard';
 import { AuthGuard } from './auth.guard';
 import { AuthComponent } from './auth/auth.component';
 import { HomeComponent } from './home/home.component';
@@ -11,7 +12,7 @@ import { ProfileComponent } from './profile/profile.component';
 const routes: Routes = [
   {path:'',component:HomeComponent},
   {path:'profile',component:ProfileComponent , canActivate:[AuthGuard]},
-  {path:'acceuil',component:AcceilComponent},
+  {path:'acceuil',component:AcceilComponent ,canActivate:[AcceuilGuard]},
   {path:'nav',component:NavlogComponent},
   {path:'authentification',component:AuthComponent}
 ];
